37309e2d3fda2c8b808672d71a73831e4c5b7691
alexschad <ags@vanguardistas.net>
2016-03-01 14:49:57 +0100

    Update ls.rias.js
    only use the url in srcset for the image src parameter.

--- plugins/rias/ls.rias.js
+++ plugins/rias/ls.rias.js
@@ -161,7 +161,7 @@
 			elem.removeAttribute(config.srcsetAttr);
 		} else {
 			elem.setAttribute(config.srcsetAttr, src.srcset.join(', '));
-			elem.setAttribute('src', src.srcset[src.srcset.length-1]);
+			elem.setAttribute('src', src.srcset[src.srcset.length-1].split(' ')[0]);
 		}
 
 		Object.defineProperty(elem, '_lazyrias', {
